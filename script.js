3f8d6e8bec6d40f5b788b8459d0a94db1986ae14
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-02-14 23:25:13 +0530

    managed frontend

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3005
 const {connectDB} = require('./connection')
 const UserRouter = require('./routes/user')
+const cors = require('cors')
 
+app.use(cors()) // middleware
+app.use(express.json()) // middleware
 app.use(express.urlencoded({extended:false})) // middleware
 
 connectDB('mongodb://127.0.0.1:27017/login_data').then(()=>{console.log("DB connected")}).catch((err)=>console.log(`Got an error`,err)) // database connected
